9c5330c22865ed97630f57dd2378494689cd153f
GitHub Action <action@github.com>
2021-09-05 15:35:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/sangyuhiter/justforarest/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sangyuhiter/justforarest/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sangyuhiter.justforarest.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sangyuhiter.justforarest.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/sangyuhiter/justforarest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/sangyuhiter/justforarest/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.sangyuhiter.justforarest.services
 
+import com.github.sangyuhiter.justforarest.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
